d96615fe5fa7586373b8f119b214a398c99a331a
Artenlaclase <artenlaclase@gmail.com>
2025-12-02 21:36:41 -0300

    chore: update Astro configuration and add Layout component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind(), icon()],
 });
