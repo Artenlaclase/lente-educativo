9cc86aaaac536dbe24664412175228759ef064af
Artenlaclase <artenlaclase@gmail.com>
2025-12-02 21:50:02 -0300

    feat: integrate Tailwind CSS and add ModulesList component with course details

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [icon()],
 });
